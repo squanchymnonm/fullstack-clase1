36ed50b64d1aabd5d8220d9001d0e4a64b780827
squanchymnonm <monaldi.nicolas@gmail.com>
2022-12-01 10:59:52 -0300

    Agrego variable sin sentido

---
+++ ClaseDebug.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$contador = 0;
+
+while (true)
+{
+    $contador = aumentarContador($contador);
+}
+
+
+
+
+function aumentarContador($contador)
+{
+    $contador++;
+    return $contador;
+}
+
--- index.php
+++ index.php
@@ -9,18 +9,61 @@
             "Luciano",
             "Raul",
             "Leonardo"
-        ]
+        ];
+
+        $peliculas = [
+                [
+                    "nombre" => "StarWars",
+                    "director" => "Goeroge Lucas"
+                ],
+                [
+                    "nombre" => "StarWars 2",
+                    "director" => "Goeroge Lucas"
+                ],
+                [
+                    "nombre" => "StarWars 3",
+                    "director" => "Goeroge Lucas"
+                ],
+                [
+                    "nombre" => "Dragon ball",
+                    "director" => "director 1"
+                ],
+                [
+                    "nombre" => "El señor de los anillos",
+                    "director" => "director 2"
+                ]
+        ];
 
 
     ?>
     <title> Este es un titulo </title>
 </head>
 <body>
-Hello <?php
-print $nombres[rand(0, count($nombres) - 1)];
 
-$new = "";
-?>
+
+
+<ul>
+    <?php
+    filtrarPorDirector($peliculas, "director 1");
+
+    ?>
+
+</ul>
+
+        <?php
+
+
+
+        function filtrarPorDirector($peliculas, $nombreDelDirector) {
+            foreach ($peliculas as $pelicula) {
+                if ($pelicula["director"] == $nombreDelDirector) {
+                    print "<li>" . $pelicula["nombre"] ;
+                }
+            }
+        }
+
+        ?>
+
 </body>
 </html1>
 
